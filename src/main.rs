4f48bb2e67ce8de36a115b547cbd5ee98af12772
mtomko <mjt0229@gmail.com>
2015-04-10 17:07:21 -0700

    Add a command-line parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-#![feature(collections)]
-#![feature(env)]
+//#![feature(collections)]
 
-use std::env;
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+use std::error::Error;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
 
 /// ```
 /// let d = complement_dna("CCGTTAG")
 /// assert!(d = "GGCAATC")
 /// ```
-fn complement_dna(dna: &str) -> Box<String> {
-    let mut comp = Box::new(String::new());
+fn complement_dna(dna: String) -> String {
+    let mut comp = String::new();
     for b in dna.chars() {
         let bc = match b {
             'A' => 'T',
@@ -26,20 +33,48 @@ fn complement_dna(dna: &str) -> Box<String> {
     comp
 }
 
-fn reverse(s: Box<String>) -> Box<String> {
-    Box::new(s.chars().rev().collect::<String>())
+fn reverse(s: String) -> String {
+    s.chars().rev().collect::<String>()
 }
 
-fn reverse_complement(dna: &str) -> Box<String> {
+fn reverse_complement(dna: String) -> String {
     reverse(complement_dna(dna))
 }
 
+static USAGE: &'static str = "
+Usage: rc (<seq>... | -f <file>)
+
+Options:
+    -f  File containing DNA sequences
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_file: String,
+    arg_seq: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
 
-    for s in args.tail() {
-        let c = reverse_complement(&s);
-        println!{"{}", c};
+    if args.arg_seq.len() > 0 {
+        for s in args.arg_seq {
+            let c = reverse_complement(s);
+            println!{"{}", c};
+        }
+    } else {
+        let path = Path::new(&args.arg_file);
+        let file = match File::open(&path) {
+            Err(why) => panic!("Unable to open {}: {}", path.display(),
+                                                        Error::description(&why)),
+            Ok(file) => file,
+        };
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let c = reverse_complement(line.unwrap());
+            println!{"{}", c};
+        }
     }
-
 }
